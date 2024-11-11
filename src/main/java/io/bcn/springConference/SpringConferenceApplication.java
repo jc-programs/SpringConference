020c4c75a8f67617dd31be26f78c38b638c17aa7
albertprofe <albert@gmail.com>
2024-11-11 12:34:31 +0100

    first commit

---
+++ src/main/java/io/bcn/springConference/SpringConferenceApplication.java
@@ -0,0 +1,13 @@
+package io.bcn.springConference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConferenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/bcn/springConference/SpringConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bcn.springConference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
