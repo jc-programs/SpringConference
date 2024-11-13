ecbccb2bcd0e944afb5284feac8b47e0fc1062f5
jc-programs <jose@gueto.com>
2024-11-13 13:32:47 +0100

    Added model class Speaker

--- src/main/java/io/bcn/springConference/model/Speaker.java
+++ src/main/java/io/bcn/springConference/model/Speaker.java
@@ -1,4 +1,24 @@
 package io.bcn.springConference.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "speakers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Speaker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+//    @ManyToOne
+//    @JoinColumn(name = "conference_id")
+//    private Conference conference;
 }
