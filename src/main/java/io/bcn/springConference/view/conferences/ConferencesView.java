9626ac9d28941d91cbaa3ff115d438e82fd1e495
jc-programs <jose@gueto.com>
2024-11-15 15:31:19 +0100

    ConferenceGrid with order and hidden id. TODO show book and speaker (but not their id)

--- src/main/java/io/bcn/springConference/view/conferences/ConferencesView.java
+++ src/main/java/io/bcn/springConference/view/conferences/ConferencesView.java
@@ -1,11 +1,13 @@
 package io.bcn.springConference.view.conferences;
 
 import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Menu;
@@ -13,7 +15,11 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import io.bcn.springConference.model.Conference;
 import io.bcn.springConference.repository.ConferenceRepository;
+import io.bcn.springConference.utilities.Views;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
 
 @PageTitle("Conferences")
 @Route("conferences")
@@ -22,7 +28,6 @@ public class ConferencesView extends Composite<VerticalLayout> {
 
     @Autowired
     ConferenceRepository conferenceRepository;
-/*
     private final Grid<Conference> conferencesGrid;
 
     private final TextField textFieldConference;
@@ -30,7 +35,7 @@ public class ConferencesView extends Composite<VerticalLayout> {
     private final DatePicker datePickerDate;
     private final TextField textFieldYoutube;
     private final TextField textFieldContent;
-    private final NumberField numberFieldDuration;
+    private final IntegerField integerFieldDuration;
     private final TextField textFieldRoom;
 
     private final Binder<Conference> binder;
@@ -41,9 +46,9 @@ public class ConferencesView extends Composite<VerticalLayout> {
         datePickerDate = new DatePicker("Date");
         textFieldYoutube = new TextField("Youtube");
         textFieldContent = new TextField("Content");
-        numberFieldDuration = new NumberField("Duration");
+        integerFieldDuration = new IntegerField("Duration");
         textFieldRoom = new TextField("Room");
-        conferencesGrid = new Grid<>(Conference.class);
+        conferencesGrid = getConferencesGrid();
 
         conferencesGrid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
         conferencesGrid.setWidth("100%");
@@ -52,17 +57,17 @@ public class ConferencesView extends Composite<VerticalLayout> {
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
         getContent().add(conferencesGrid);
-        getContent().add(getRowConferenceFields());
+        getContent().add(getRow1ConferenceFields());
+        getContent().add(getRow2ConferenceFields());
         getContent().add(getRowConferenceButtons());
 
         binder = new Binder<>(Conference.class);
         binder.bind(textFieldConference, Conference::getConference, Conference::setConference);
         binder.bind(textFieldTitle, Conference::getTitle, Conference::setTitle);
-        binder.forField(datePickerDate).bind(Conference::getDate, Conference::setDate);
         binder.bind(datePickerDate, Conference::getDate, Conference::setDate);
         binder.bind(textFieldYoutube, Conference::getYoutube, Conference::setYoutube);
         binder.bind(textFieldContent, Conference::getContent, Conference::setContent);
-        binder.bind(numberFieldDuration, Conference::getDuration, Conference::setDuration);
+        binder.bind(integerFieldDuration, Conference::getDuration, Conference::setDuration);
         binder.bind(textFieldRoom, Conference::getRoom, Conference::setRoom);
 
         conferencesGrid.asSingleSelect().addValueChangeListener(
@@ -76,5 +81,101 @@ public class ConferencesView extends Composite<VerticalLayout> {
         );
     }
 
- */
+    private Grid<Conference> getConferencesGrid(){
+//        Grid<Conference> grid = new Grid<>(Conference.class);
+        Grid<Conference> grid = new Grid<>(Conference.class,false);
+
+        Grid.Column<Conference> col = grid.addColumn(Conference::getId);
+        col.setVisible(false);
+        grid.addColumn(Conference::getConference).setHeader("Conference");
+        grid.addColumn(Conference::getTitle).setHeader("Title");
+        grid.addColumn(Conference::getDate).setHeader("Date");
+        grid.addColumn(Conference::getYoutube).setHeader("Youtube");
+        grid.addColumn(Conference::getContent).setHeader("Content");
+        grid.addColumn(Conference::getDuration).setHeader("Duration");
+        grid.addColumn(Conference::getRoom).setHeader("Room");
+
+        grid.getColumns().forEach( column -> column.setSortable(true) );
+
+        return grid;
+    }
+
+
+    @PostConstruct
+    private void setGridData() {
+        if (conferenceRepository != null) {
+            conferencesGrid.setItems(conferenceRepository.findAll());
+        }
+    }
+
+    private void saveConference() {
+        if (conferenceRepository == null) {
+            return;
+        }
+
+        Conference conference = binder.getBean();
+        if( conference == null ){
+            conference = new Conference();
+        }
+        if( conference.getId() == null ){
+            conference.setId(UUID.randomUUID());
+        }
+        conferenceRepository.save(conference);
+        clearConferenceForm();
+        setGridData();
+    }
+
+    private void deleteConference(){
+        if( conferenceRepository == null ){
+            return;
+        }
+
+        Conference conference = binder.getBean();
+        UUID uuid = conference.getId();
+        if( uuid != null ){
+            conferenceRepository.deleteById(uuid);
+            clearConferenceForm();
+            setGridData();
+        }
+    }
+
+    private void clearConferenceForm(){
+        binder.setBean(new Conference());
+    }
+
+
+    private HorizontalLayout getRow1ConferenceFields(){
+        HorizontalLayout row = Views.getNewRow();
+
+        row.add(textFieldConference);
+        row.add(textFieldTitle);
+        row.add(datePickerDate);
+
+        return row;
+    }
+
+    private HorizontalLayout getRow2ConferenceFields(){
+        HorizontalLayout row = Views.getNewRow();
+
+        row.add(textFieldYoutube);
+        row.add(textFieldContent);
+        row.add(integerFieldDuration);
+        row.add(textFieldRoom);
+
+        return row;
+    }
+
+    private HorizontalLayout getRowConferenceButtons() {
+        HorizontalLayout row = Views.getNewRow();
+
+        Button buttonSave = new Button("Insert / Update", clickEvent -> { saveConference(); });
+        Button buttonDelete = new Button("Delete", clickEvent -> { deleteConference(); });
+
+        row.add(buttonSave);
+        row.add(buttonDelete);
+
+        return row;
+    }
+
+
 }
