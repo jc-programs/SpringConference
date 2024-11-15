94227289cee3ebe74a3538d83c71a649ef52eb2c
jc-programs <jose@gueto.com>
2024-11-15 13:17:32 +0100

    ConferenceRepository

---
+++ src/main/java/io/bcn/springConference/repository/ConferenceRepository.java
@@ -0,0 +1,8 @@
+package io.bcn.springConference.repository;
+
+import io.bcn.springConference.model.Conference;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ConferenceRepository extends JpaRepository<Conference, UUID> {}
